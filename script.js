b10114ee2589c1c6c6a66f7545d7008126f28a21
RobCMYK <59516575+RobCMYK@users.noreply.github.com>
2023-09-05 20:15:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+  
+  $('#theNuke').click( 
+    function(
+    ){
+      //code goes here
+      console.log("position: ", $( '#theNuke' ).position() );
+    }
+  );
+  
+  // if(   $( '#theNuke' ).position().left > 500) {
+  //   console.log("Wus poppin' B")
+  //})
+    
+  
+  // $( "#theNuke" ).draggable({stop: function(){
+  //     if(   $( '#theNuke' ).position().left > 500) {
+  //    console.log("Wus poppin' B");
+  //   }
+  //   }
+    
+    
+  // });
+  
+  
+  });
